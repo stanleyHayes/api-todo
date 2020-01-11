40892d26daa580501ef258cbe2fcc84a43f27f06
Stanley Hayford <hayfordstanley@gmail.com>
2020-01-11 08:03:52 +0000

    Added index.js file and filled it with boilerplate express app code

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+
+app.listen(process.env.PORT,function () {
+    console.log(`Server connected on PORT ${process.env.PORT}`);
+});
