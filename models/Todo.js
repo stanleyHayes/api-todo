3f3dc0fcb9e5fb27eccb07fc5e6b552620ef9b0b
Stanley Hayford <hayfordstanley@gmail.com>
2020-01-11 10:12:36 +0000

    Added models module. Todo Schema and User Schema

---
+++ models/Todo.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const TodoSchema = new Schema({
+    owner: {
+        required: true,
+        ref: "User",
+        type: Schema.Types.ObjectId
+    },
+    action: {
+        required: true,
+        type: String
+    },
+    date_created: {
+        type: Date,
+        default: Date.now()
+    },
+    status: {
+        type: Boolean,
+        default: false
+    }
+});
+
+const Todo = mongoose.model("Todo", TodoSchema);
+
+module.exports = Todo;
